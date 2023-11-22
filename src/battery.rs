11c2f25fb964b21b05030396eaeb0b6f7e8d12f0
prace@srbpavel.cz <prace@srbpavel.cz>
2023-11-22 14:37:31 +0100

    just trying variants

--- src/battery.rs
+++ src/battery.rs
@@ -5,6 +5,10 @@ use log::info;
 #[allow(unused_imports)]
 use log::warn;
 
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
+
+use esp_idf_hal::gpio::IOPin;
 use esp_idf_hal::gpio::ADCPin;
 use esp_idf_hal::peripheral::Peripheral;
 
@@ -16,16 +20,279 @@ use esp_idf_hal::adc::AdcDriver;
 use esp_idf_hal::delay::FreeRtos;
 
 //const BATTERY_VOLTAGE: f32 = 3.7;
-const VOLTAGE_COEFICIENT: f32 = 5.114;
+//const BATTERY_VOLTAGE: f32 = 4.2;
 //const VOLTAGE_DIVIDER: &str = "4.1387v : 0.809 = 5.114 coeficient";
+const VOLTAGE_COEFICIENT: f32 = 5.114;
 
 const REPETITION: u8 = 10;
+// todo!() conf of + fn
 const DELAY_MEASUREMENT_MS: u32 = 100; // 1000
 
+/*
+pub enum Status {
+    Init,
+    Done,
+    Failed,
+}
+*/
+
+#[allow(unused)]
 //
-pub fn measure<const ATTN: u32, PIN, ADC>(
+// https://users.rust-lang.org/t/how-to-store-a-trait-as-field-of-a-struct/87762/2
+//
+pub struct Battery<PIN, ADC, const ATTN: u32> {
+    pub gpio: PIN,
+    // todo!() -> try harder:
+    // as we can measure 3or more bateries we need access to adc1 or adc2 
+    // at multiple places -> ? Arc<Mutex> ???
+    //pub adc: ADC,
+    pub adc: std::sync::Arc<std::sync::Mutex<ADC>>,
+    //pub adc: std::sync::Arc<ADC>,
+    pub delay_ms: u32,
+    //pub receiver: Receiver<Command>,
+    pub receiver: std::sync::Arc<std::sync::Mutex<Receiver<Command>>>,
+    pub sender: Sender<Measurement>,
+}
+
+impl<PIN, ADC, const ATTN: u32> Battery<PIN, ADC, ATTN>
+where
+    ADC: Adc + Peripheral<P = ADC> + 'static, <ADC as Peripheral>::P: Adc,
+    PIN: IOPin + ADCPin<Adc = ADC>,
+{
+    //
+    pub fn init(self) {
+        std::thread::spawn(move || {
+            let pin_id = self.gpio.pin();
+            let adc_channel_driver: Result<AdcChannelDriver::< ATTN, _>, esp_idf_sys::EspError> = AdcChannelDriver::new(self.gpio);
+            
+            match adc_channel_driver {
+                Ok(mut cd) => {
+                    let adc_driver = AdcDriver::new(
+                        //self.adc,
+                        self.adc.lock().unwrap(), // NOT SAFE !!! todo
+                        &adc::config::Config::new().calibration(true),
+                    );
+                    
+                    if let Ok(mut d) = adc_driver {
+                        //while let Ok(channel_data) = self.receiver.recv() {
+                        while let Ok(channel_data) = self.receiver.lock().unwrap().recv() {
+                            match channel_data {
+                                Command::Measure => {
+                                    let mut measurement = Measurement::default();
+                                    // todo!() via conf?
+                                    let mut counter = 0;
+                                    let mut values = Vec::new();
+                                    
+                                    while counter < REPETITION {
+                                        counter += 1;
+                                        
+                                        match d.read(&mut cd) {
+                                            Ok(value) => {
+                                                // DEBUG
+                                                warn!("$$$ PIN: {pin_id} [{counter:03}] {value} mV");
+                                                values.push(value);
+                                                
+                                            },
+                                            Err(_e) => {
+                                                // todo!()
+                                            },
+                                        }
+                                        
+                                        FreeRtos::delay_ms(self.delay_ms);
+                                    }
+                                    
+                                    let average = values
+                                        .iter()
+                                        .sum::<u16>() as f32
+                                        / (values.len() as f32);
+                                    
+                                    measurement.voltage = average * VOLTAGE_COEFICIENT;
+                                    
+                                    // DEBUG
+                                    warn!("### average: {}mV * coef: {} -> {}",
+                                          average,
+                                          VOLTAGE_COEFICIENT,
+                                          measurement.voltage,
+                                    );
+                                    
+                                    // send measurement
+                                    if let Err(_e) = self.sender.send(measurement) {
+                                        // todo!()
+                                    }
+                                },
+                            }
+                        }
+                    }
+                },
+                Err(_e) => {
+                    // todo!()
+                },
+            };
+        });
+    }
+}
+
+pub enum Command {
+    Measure
+}
+
+#[derive(Debug)]
+pub struct Measurement {
+    //pub id: u8,
+    pub voltage: f32,
+    //pub gpio: Option<String>,
+    //pub adc: Option<String>,
+    //pub status: Status,
+}
+
+impl Default for Measurement {
+    //
+    fn default() -> Self {
+        Self {
+            //id: 0,
+            voltage: 0.0,
+            //gpio: None,
+            //adc: None,
+            //status: Status::Init,
+        }
+    }
+}
+
+//
+#[allow(unused)]
+pub fn start_via_pin<PIN, ADC, const ATTN: u32>(
+    gpio: PIN,
+    adc_peripheral: ADC,
+    delay_ms: u32,
+    receiver: Receiver<Command>,
+    sender: Sender<Measurement>
+)
+where
+    ADC: Adc + Peripheral<P = ADC> + 'static, <ADC as Peripheral>::P: Adc,
+    PIN: IOPin + ADCPin<Adc = ADC>,
+{
+    std::thread::spawn(move || {
+        let pin_id = gpio.pin();
+        let adc_channel_driver: Result<AdcChannelDriver::< ATTN, _>, esp_idf_sys::EspError> = AdcChannelDriver::new(gpio);
+
+        match adc_channel_driver {
+            Ok(mut cd) => {
+                let adc_driver = AdcDriver::new(
+                    adc_peripheral,
+                    &adc::config::Config::new().calibration(true),
+                );
+
+                if let Ok(mut d) = adc_driver {
+                    while let Ok(channel_data) = receiver.recv() {
+                        match channel_data {
+                            Command::Measure => {
+                                let mut measurement = Measurement::default();
+                                // todo!() via conf?
+                                let mut counter = 0;
+                                let mut values = Vec::new();
+                                
+                                while counter < REPETITION {
+                                    counter += 1;
+                                    
+                                    match d.read(&mut cd) {
+                                        Ok(value) => {
+                                            // DEBUG
+                                            warn!("$$$ PIN: {pin_id} [{counter:03}] {value} mV");
+                                            values.push(value);
+                                          
+                                        },
+                                        Err(_e) => {
+                                            // todo!()
+                                        },
+                                    }
+                                    
+                                    FreeRtos::delay_ms(delay_ms);
+                                }
+
+                                let average = values
+                                    .iter()
+                                    .sum::<u16>() as f32
+                                    / (values.len() as f32);
+                                
+                                measurement.voltage = average * VOLTAGE_COEFICIENT;
+                               
+                                // DEBUG
+                                warn!("### average: {}mV * coef: {} -> {}",
+                                      average,
+                                      VOLTAGE_COEFICIENT,
+                                      measurement.voltage,
+                                );
+
+                                // send measurement
+                                if let Err(_e) = sender.send(measurement) {
+                                    // todo!()
+                                }
+                            },
+                        }
+                    }
+                }
+            },
+            Err(_e) => {
+                // todo!()
+            },
+        };
+    });
+}
+    
+//
+#[allow(unused)]
+pub fn measure_pin<PIN, ADC, const ATTN: u32>(gpio: PIN,
+                                              adc_peripheral: ADC,
+) -> Result<(), esp_idf_sys::EspError>
+where
+    ADC: Adc + Peripheral<P = ADC>, <ADC as Peripheral>::P: Adc,
+    PIN: esp_idf_hal::gpio::IOPin + ADCPin<Adc = ADC>,
+{
+    let pin_id = gpio.pin();
+
+    let mut adc_channel_driver: AdcChannelDriver::< ATTN, _> = AdcChannelDriver::new(gpio)?;
+
+    let mut adc_driver = AdcDriver::new(
+        adc_peripheral,
+        &adc::config::Config::new().calibration(true),
+    )?;
+
+    let mut measurement = Measurement::default();
+    
+    let mut counter = 0;
+    let mut values = Vec::new();
+    while counter < REPETITION {
+        counter += 1;
+        
+        let value = adc_driver.read(&mut adc_channel_driver)?;
+
+        warn!("$$$ PIN: {pin_id} [{counter:03}] {value} mV");
+        values.push(value);
+        
+        FreeRtos::delay_ms(DELAY_MEASUREMENT_MS);
+    }
+
+    let average = values
+        .iter()
+        .sum::<u16>() as f32
+        / (values.len() as f32);
+    
+    measurement.voltage = average * VOLTAGE_COEFICIENT;
+    
+    warn!("$$$ ADC -> average: {} mV / {}",
+          average,
+          measurement.voltage,
+    );
+    
+    Ok(())
+}
+
+//
+#[allow(unused)]
+pub fn measure_channel_driver<const ATTN: u32, PIN, ADC>(
     adc_channel_driver: &mut AdcChannelDriver<ATTN, PIN>,
     adc_peripheral: ADC,
+    //adc_peripheral: &mut ADC,
 ) -> Result<(), esp_idf_sys::EspError> 
 where
     ADC: Adc + Peripheral<P = ADC>, <ADC as Peripheral>::P: Adc,
@@ -36,31 +303,33 @@ where
         &adc::config::Config::new().calibration(true),
     )?;
 
-    let mut measurement_counter = 0;
-    let mut measurement_values = Vec::new();
+    let mut measurement = Measurement::default();
     
-    while measurement_counter < REPETITION {
-        measurement_counter += 1;
+    let mut counter = 0;
+    let mut values = Vec::new();
+    while counter < REPETITION {
+        counter += 1;
         
-        let measurement = adc_driver.read(adc_channel_driver)?;
+        let value = adc_driver.read(adc_channel_driver)?;
 
-        warn!("$$$ ADC?-?: [{measurement_counter:03}] {measurement} mV");
-        measurement_values.push(measurement);
+        warn!("$$$ ADC?-?: [{counter:03}] {value} mV");
+        values.push(value);
         
         FreeRtos::delay_ms(DELAY_MEASUREMENT_MS);
     }
 
-    let average = measurement_values
+    let average = values
         .iter()
         .sum::<u16>() as f32
-        / (measurement_values.len() as f32);
+        / (values.len() as f32);
     
-    let voltage = average * VOLTAGE_COEFICIENT;
+    measurement.voltage = average * VOLTAGE_COEFICIENT;
     
     warn!("$$$ ADC -> average: {} mV / {}",
           average,
-          voltage,
+          measurement.voltage,
     );
+
     
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -7,56 +7,199 @@ use log::info;
 #[allow(unused_imports)]
 use log::warn;
 
+use std::sync::mpsc::channel;
+
+use embedded_hal::blocking::delay::DelayMs;
+
 use esp_idf_sys as _;
 
-use esp_idf_hal::adc::AdcChannelDriver;
 use esp_idf_hal::adc::attenuation;
+//use esp_idf_hal::adc::AdcChannelDriver;
 
+
+// todo!() -> Config
 const MACHINE_NAME: &str = "peasant";
+
+const DELAY_SLEEP_DURATION_MS: u32 = 10*1000;
 
 const ATTN_ONE: u32 = attenuation::DB_11;
 const ATTN_TWO: u32 = attenuation::DB_11;
+
 
 //
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
     info!("machine: {MACHINE_NAME} -> rust_esp32_std_on_battery");
-   
+
+    let mut sleep = esp_idf_hal::delay::FreeRtos {};
+    
     // PERIPHERALS
     //
     let peripherals = esp_idf_hal::peripherals::Peripherals::take().unwrap();
     let adc_1 = peripherals.adc1;
+    let adc_1 = std::sync::Arc::new(std::sync::Mutex::new(adc_1));
     let adc_2 = peripherals.adc2;
+    let adc_2 = std::sync::Arc::new(std::sync::Mutex::new(adc_2));
 
-    // todo!() -> channel/...
+    //let mut adc_1 = peripherals.adc1;
+
+    // CHANNEL
+    let (command_sender, command_receiver) =
+        channel::<battery::Command>();
+
+    let command_receiver = std::sync::Arc::new(std::sync::Mutex::new(command_receiver));
+    
+    let (measurement_sender, measurement_receiver) =
+        channel::<battery::Measurement>();
+    /*
+    let (command_sender_one, command_receiver_one) =
+        channel::<battery::Command>();
+
+    let (command_sender_two, command_receiver_two) =
+        channel::<battery::Command>();
+
+    let (measurement_sender_one, measurement_receiver_one) =
+        channel::<battery::Measurement>();
+    */
     
     // PINS
     //
+    // ADC1
     //let pin_adc_0 = peripherals.pins.gpio0; // ADC1-0 GPIO0
     //let pin_adc_1 = peripherals.pins.gpio1; // ADC1-1 GPIO1
     //let pin_adc_2 = peripherals.pins.gpio2; // ADC1-2 GPIO2
     let pin_adc_3 = peripherals.pins.gpio3; // ADC1-3 GPI03 
-    //let pin_adc_4 = peripherals.pins.gpio4; // ADC1-4 GPI04 
+    let pin_adc_4 = peripherals.pins.gpio4; // ADC1-4 GPI04 
+    // ADC2
     let pin_adc_5 = peripherals.pins.gpio5; // ADC2-0 GPIO5    
 
     // MEASUREMENT
     //
     // 1
+    //
+    // INFINITE
+    //
+    // todo!() -> via conf?
+    /*
+    let delay_measurement_ms: u32 = 100;
+    battery::start_via_pin::<_, _, ATTN_ONE>(
+        pin_adc_4,
+        adc_1,
+        delay_measurement_ms,
+        command_receiver_one,
+        measurement_sender_one.clone(),
+    );
+    */
+
+    let battery_one_3: battery::Battery<_, _, ATTN_ONE> = battery::Battery {
+        gpio: pin_adc_3,
+        adc: adc_1.clone(),
+        delay_ms: 100,
+        //receiver: command_receiver_one,
+        receiver: command_receiver.clone(),
+        //sender: measurement_sender_one.clone(),
+        sender: measurement_sender.clone(),
+    };
+
+    battery_one_3.init();
+
+    let battery_one_4: battery::Battery<_, _, ATTN_ONE> = battery::Battery {
+        gpio: pin_adc_4,
+        adc: adc_1.clone(),
+        delay_ms: 100,
+        //receiver: command_receiver_one,
+        receiver: command_receiver.clone(),
+        //sender: measurement_sender_one.clone(),
+        sender: measurement_sender.clone(),
+    };
+
+    battery_one_4.init();
+    
+    let battery_two_5: battery::Battery<_, _, ATTN_TWO> = battery::Battery {
+        gpio: pin_adc_5,
+        //adc: adc_2,
+        //adc: std::sync::Arc::new(std::sync::Mutex::new(adc_2)),
+        adc: adc_2.clone(),
+        delay_ms: 200,
+        //receiver: command_receiver_two,
+        receiver: command_receiver.clone(),
+        //sender: measurement_sender_one.clone(),
+        sender: measurement_sender.clone(),
+    };
+
+    battery_two_5.init();
+
+    /*
+    let battery_three: battery::Battery<_, _, ATTN_ONE> = battery::Battery {
+        gpio: pin_adc_3,
+        adc: adc_1,
+        delay_ms: 300,
+        receiver: command_receiver_one,
+        sender: measurement_sender_one.clone(),
+    };
+
+    battery_two.init();
+    */
+    
+    // ONCE
+    /*
+    // via AdcChannelDriver    
     let mut adc_channel_driver_one: AdcChannelDriver::<ATTN_ONE, _> = AdcChannelDriver::new(pin_adc_3)?;
 
-    if let Ok(()) = battery::measure(&mut adc_channel_driver_one,
-                                     adc_1,
+    if let Err(_e) = battery::measure_channel_driver(&mut adc_channel_driver_one,
+                                                     adc_1,
+                                                     //&mut adc_1,
     ) {}
-
-    // 2
-    let mut adc_channel_driver_two: AdcChannelDriver::<ATTN_TWO, _> = AdcChannelDriver::new(pin_adc_5)?;
-
-    if let Ok(()) = battery::measure(&mut adc_channel_driver_two,
-                                     adc_2,
-    ) {}
+    */
     
+    /*
+    // 2
+    // via Gpio
+    if let Err(_e) = battery::measure_pin::<_, _, ATTN_TWO>(pin_adc_5,
+                                                            adc_2,
+    ) {}
+    */
+    
+    // MEASUREMENT display/parse/mqtt/...
+    std::thread::spawn(move || {
+        //while let Ok(channel_data) = measurement_receiver_one.recv() {
+        while let Ok(channel_data) = measurement_receiver.recv() {
+            info!("MEASUREMENT value: {:?}",
+                  channel_data,
+            );
+        }
+    });
+    //
+
+    // todo!() -> prepare for mqtt request
+    // try harder !!!
+    // LOOP
+    std::thread::spawn(move || {
+        loop {
+            if let Err(e) = command_sender
+                .clone()
+                .send(battery::Command::Measure) {
+                    
+                    error!("### Error: One send(Command::Measure) -> {e:?}");
+                }
+
+            /*
+            if let Err(e) = command_sender_two
+                .clone()
+                .send(battery::Command::Measure) {
+                    
+                    error!("### Error: Two send(Command::Measure) -> {e:?}");
+                }
+            */
+            
+            sleep.delay_ms(DELAY_SLEEP_DURATION_MS);
+        }
+    });
+
     // todo!() -> deep_sleep
+    // via config measure once + sleep
+    // via config infinite measuare
     
     Ok(())
 }
